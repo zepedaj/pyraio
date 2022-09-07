55c3c8ab4c60931d94527856c331e4c12d02de4c
Joaquin Zepeda <jazs.pro@gmail.com>
2022-09-07 10:33:58 -0700

    PyRaio dry-ness; vanilla dataset keywords.

--- pyraio/util.pyx
+++ pyraio/util.pyx
@@ -4,14 +4,6 @@ import os
 from contextlib import contextmanager
 import errno
 
-ctypedef struct buf_meta_t:
-    size_t data_start # Data start position in aligned buffer
-    size_t data_end # Data end position in aligned buffer
-    int fd # The original file descriptor
-    size_t offset # The original offset requested
-    size_t num_bytes # The original num bytes requested
-    PyObject *ref # A reference supplied by the user.
-
 cdef str buf_meta_t_str(buf_meta_t &buf_meta):
     filename = os.readlink(f'/proc/self/fd/{buf_meta.fd}')
     return f"<{filename}, offset={buf_meta.offset}, num_bytes={buf_meta.num_bytes} | data_start={buf_meta.data_start}, data_end={buf_meta.data_end}>"
